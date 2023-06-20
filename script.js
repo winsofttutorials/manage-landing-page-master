a5e823cab993454a31e07970d14f6bb276a0f6fc
winsofttutorials <winsofttutorials@gmail.com>
2023-06-20 15:34:38 +0100

    'responsiveTest'

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ menuIcons.addEventListener("click", () => {
   closeIcons.style.display = "block";
   menuIcons.style.display = "none";
   links.style.display = "block";
+  document.querySelector(".container").style.backgroundColor = "#1616160E";
 });
 
 closeIcons.addEventListener("click", () => {
   closeIcons.style.display = "none";
   menuIcons.style.display = "block";
   links.style.display = "none";
+  document.querySelector(".container").style.backgroundColor = "";
 });
 
 document.querySelectorAll(".nav-links").forEach((n) =>
@@ -20,5 +22,29 @@ document.querySelectorAll(".nav-links").forEach((n) =>
     menuIcons.style.display = "block";
     links.style.display = "none";
     closeIcons.style.display = "none";
+    document.querySelector(".container").style.backgroundColor = "";
   })
 );
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+    },
+    "@1.10": {
+      slidesPerView: 3,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
