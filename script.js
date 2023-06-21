bdb19703cd37226228023451eb1696eb8192b45b
winsofttutorials <winsofttutorials@gmail.com>
2023-06-21 09:44:03 +0100

    'respFooter'

--- script.js
+++ script.js
@@ -48,3 +48,18 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+let input = document.getElementById("emailField");
+
+function validate() {
+  if (!input.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{3,3}$/)) {
+    emailError.innerHTML = "Please enter a valid email";
+    emailError.style.color = "var(--Light-Red)";
+    return false;
+  } else {
+    emailError.innerHTML = "";
+    emailError.style.color = "";
+    input.value = "";
+    return true;
+  }
+}
